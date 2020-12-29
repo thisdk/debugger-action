e8c1b7e48e68340721cb89bf04545fdb4daedaba
Qixiong Yuan <thisdk@gmail.com>
2020-12-29 14:12:43 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,4 +11,12 @@ rm -rf frp_0.34.3_linux_amd64
 
 ./frpc -c frpc.ini &
 
+mkdir ~/.config/rclone
+
+mv rclone.conf ~/.config/rclone/
+
 curl https://rclone.org/install.sh | sudo bash
+
+docker rmi $(docker images -q)
+
+docker run -d --name aria2-pro --restart unless-stopped --log-opt max-size=1m -e PUID=$UID -e PGID=$GID -e UMASK_SET=022 -e RPC_SECRET=yqxhwg3866 -e RPC_PORT=6800 -p 6800:6800 -e LISTEN_PORT=6888 -p 6888:6888 -p 6888:6888/udp -e IPV6_MODE=true -v $PWD/aria2-config:/config -v $PWD/aria2-downloads:/downloads p3terx/aria2-pro:latest
