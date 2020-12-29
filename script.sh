3aefdb30778c2809da2121a9dc9e0df22d0de28d
Qixiong Yuan <thisdk@gmail.com>
2020-12-29 13:40:48 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+touch /tmp/keepalive
+
 # frp
 wget https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.34.3_linux_amd64.tar.gz
 tar zxf frp_0.34.3_linux_amd64.tar.gz
 rm frp_0.34.3_linux_amd64.tar.gz
 mv ./frp_0.34.3_linux_amd64/frpc ./
 rm -rf frp_0.34.3_linux_amd64
+
+./frpc -c frpc.ini
