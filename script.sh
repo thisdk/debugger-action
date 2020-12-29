46b999260e8232ff1060a44ffa2fc4c841941941
Qixiong Yuan <thisdk@gmail.com>
2020-12-29 15:54:42 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ rm -rf frp_0.34.3_linux_amd64
 
 sudo mkdir /root/.config/rclone
 
-mv rclone.conf /root/.config/rclone/
+sudo mv rclone.conf /root/.config/rclone/
 
 curl https://rclone.org/install.sh | sudo bash
 
@@ -23,7 +23,9 @@ docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
 
-docker run -d --name aria2-pro --restart unless-stopped --log-opt max-size=1m -e PUID=$UID -e PGID=$GID -e UMASK_SET=022 -e RPC_SECRET=yqxhwg3866 -e RPC_PORT=6800 -p 6800:6800 -e LISTEN_PORT=6888 -p 6888:6888 -p 6888:6888/udp -e IPV6_MODE=true -v $PWD/aria2-config:/config -v $PWD/aria2-downloads:/downloads -v $PWD/aria2-offline:/offline p3terx/aria2-pro:latest
+sudo mkdir /mnt/offline
 
-sudo /usr/bin/rclone mount google:offline $PWD/aria2-offline --allow-non-empty &
+sudo /usr/bin/rclone mount google:offline /mnt/offline --allow-non-empty &
+
+docker run -d --name aria2-pro --restart unless-stopped --log-opt max-size=1m -e PUID=$UID -e PGID=$GID -e UMASK_SET=022 -e RPC_SECRET=yqxhwg3866 -e RPC_PORT=6800 -p 6800:6800 -e LISTEN_PORT=6888 -p 6888:6888 -p 6888:6888/udp -e IPV6_MODE=true -v $PWD/aria2-config:/config -v $PWD/aria2-downloads:/downloads -v /mnt/offline:/offline p3terx/aria2-pro:latest
 
