0495ed63b2a7ae973582a793afe3e4867a3e9a1b
Qixiong Yuan <thisdk@gmail.com>
2020-12-29 13:33:08 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# frp
+wget https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.34.3_linux_amd64.tar.gz
+tar zxf frp_0.34.3_linux_amd64.tar.gz
+rm frp_0.34.3_linux_amd64.tar.gz
+mv ./frp_0.34.3_linux_amd64/frpc ./
+rm -rf frp_0.34.3_linux_amd64
